0265dfdb5e2a243852cfd39d99df0935a9fcc0ef
Serene <unexpecte-d@hotmail.com>
2019-10-16 17:00:12 +0800

    Refactor language selector

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
 import ColourContext from '../contexts/ColourContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends Component {
 	state = {
@@ -13,11 +14,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="ui container">
-				<div>
-					Select a language:
-					<i className="flag us" onClick={() => this.onLanguageChange('english')} />
-					<i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
-				</div>
+				<LanguageSelector onLanguageChange={this.onLanguageChange} />
 				<ColourContext.Provider value="red">
 					<LanguageContext.Provider value={this.state.language}>
 						<UserCreate />
